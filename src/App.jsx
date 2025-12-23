3e42fbc7b3e0960ca4ef75655748deb85c010d9d
Jordan King <105494965+nomarjay@users.noreply.github.com>
2025-12-23 16:31:25 -0500

    apple-clone initial commit navbar and hero implemented

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero/>
+    </main>
+
+  )
+}
+
+export default App
