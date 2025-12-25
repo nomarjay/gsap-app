45fa6f3a030cfdeed3e716ce5f60f1ab7d6aaad0
Jordan King <105494965+nomarjay@users.noreply.github.com>
2025-12-24 19:38:13 -0500

    implement macbook model sections

---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import useMacbookStore from '../store';
+import clsx from 'clsx';
+import { Canvas } from '@react-three/fiber';
+import { Box, OrbitControls } from '@react-three/drei';
+import MacbookModel14 from './models/Macbook-14';
+import MacbookModel16 from './models/Macbook-16';
+import MacbookModel from './models/Macbook';
+
+import StudioLights from './three/StudioLights';
+import ModelSwitcher from './three/ModelSwitcher.jsx'
+import { useMediaQuery } from 'react-responsive';
+
+const ProductViewer = () => {
+    const { color, setColor, scale, setScale } = useMacbookStore();
+    const isMobile = useMediaQuery({query: 'max-width: 1024px'})
+
+  return (
+    <section id="product-viewer">
+      <h2 className="text-center">Take a closer look.</h2>
+
+      <div className="controls">
+        <p className="info text-center">
+          Macbook Pro | Available in 14" & 16" in Space Grey & Dark Grey
+        </p>
+
+        <div className="flex-center gap-5 mt-5">
+          <div className="color-control">
+            <div
+              onClick={() => setColor('#adb5bd')}
+              className={clsx(
+                'bg-neutral-300',
+                color === '#adb5bd' && 'active'
+              )}
+            ></div>
+            <div
+              onClick={() => setColor('#2c2c2e')}
+              className={clsx(
+                'bg-neutral-900',
+                color === '#2c2c2e' && 'active'
+              )}
+            ></div>
+          </div>
+
+          <div className="size-control">
+            <div
+              onClick={() => setScale(0.06)}
+              className={clsx(
+                'bg white text-black',
+                scale === 0.06
+                  ? 'bg-white text black'
+                  : 'bg-transparent text-white'
+              )}
+            >
+              <p>14"</p>
+            </div>
+            <div
+              onClick={() => setScale(0.08)}
+              className={clsx(
+                'bg white text-black',
+                scale === 0.08
+                  ? 'bg-white text black'
+                  : 'bg-transparent text-white'
+              )}
+            >
+              <p>16"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Canvas
+        id="canvas"
+        camera={{
+          position: [0, 2, 5],
+          fov: 50,
+          near: 0.1,
+          far: 100,
+        }}
+      >
+        <StudioLights intensity={1} />
+                            
+        <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
+      </Canvas>
+    </section>
+  );
+};
+
+export default ProductViewer;
