e889b4eb46a2fec1eba786740c1d25c4de5ed2d7
Jordan King <105494965+nomarjay@users.noreply.github.com>
2025-12-24 19:38:13 -0500

    implement macbook model sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import ProductViewer from './components/ProductViewer';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
-      <Hero/>
+      <Hero />
+      <ProductViewer />
     </main>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
-import { navLinks } from '../constants'
-
+import React from 'react';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
-    return (
-        <header>
-            <nav>
-                <img src="/logo.svg" alt="Apple logo" />
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple logo" />
 
-                <ul>
-                    {navLinks.map(({ label }) => (
-                        <li key={label}>
-                            <a href={label}>{label}</a>
-                        </li>
-                    ))}
-                </ul>
+        <ul>
+          {navLinks.map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
 
-                <div className="flex-center gap-3">
-                    <button>
-                        <img src="/search.svg" alt="Search" />
-                    </button>
-                    <button>
-                        <img src="/cart.svg" alt="cart" />
-                    </button>
-                </div>
-            </nav>
-            
-      </header>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
